2dddb4883736cabf4c51a64684d704de7658bbe3
Abdallah Mostafa <abdallahmostafa6884@gmail.com>
2025-10-04 22:57:41 +0300

    User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,13 @@
+// const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema({
+//   username: { type: String, required: true, unique: true },
+//   password: { type: String, required: true }
+// });
+
+// module.exports = mongoose.model("User", userSchema);
+// ................................................................
+
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
@@ -6,4 +16,3 @@ const userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("User", userSchema);
-
