cd0e0a576a901655878f87037420e7612677c86c
Abdallah Mostafa <abdallahmostafa6884@gmail.com>
2025-10-07 17:05:51 +0300

    Book.js

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,16 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const bookSchema = new mongoose.Schema({
-//   title: { type: String, required: true },
-//   author: { type: String, required: true },
-//   year: { type: Number, required: true }
-// });
-
-// module.exports = mongoose.model("Book", bookSchema);
-
-// ........................................................................................................
-
-
 const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
