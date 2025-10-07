3e7e96aa7c9deedbfc2aaa5a9af04e1c9cbe5940
Abdallah Mostafa <abdallahmostafa6884@gmail.com>
2025-10-07 17:11:14 +0300

    auth.test.js

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,39 +1,3 @@
-// const request = require("supertest");
-// const app = require("../src/app");
-// const mongoose = require("mongoose");
-// const User = require("../src/models/User");
-
-// beforeAll(async () => {
-//   await User.deleteMany({});
-// });
-
-// afterAll(async () => {
-//   await mongoose.connection.close();
-// });
-
-// describe("Auth API", () => {
-//   it("should register a user", async () => {
-//     const res = await request(app).post("/api/auth/register").send({
-//       username: "testuser",
-//       password: "password123"
-//     });
-//     expect(res.statusCode).toBe(200);
-//     expect(res.text).toBe("User registered");
-//   });
-
-//   it("should login and return token", async () => {
-//     const res = await request(app).post("/api/auth/login").send({
-//       username: "testuser",
-//       password: "password123"
-//     });
-//     expect(res.statusCode).toBe(200);
-//     expect(res.body).toHaveProperty("token");
-//   });
-// });
-// .....................................................................................
-
-
-
 const request = require("supertest");
 const app = require("../src/app");
 const mongoose = require("mongoose");
@@ -67,12 +31,11 @@ describe("Auth API", () => {
   });
 });
 
-// ✅ Test للتأكد إن صفحة frontend شغالة
 describe("Frontend Static Files", () => {
   it("should serve index.html", async () => {
     const res = await request(app).get("/index.html");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain("Register"); // أي كلمة من الصفحة
+    expect(res.text).toContain("Register");
   });
 });
 
