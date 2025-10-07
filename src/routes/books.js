21cc869bb1393e5c6a2ac970f294c77ef924d72d
Abdallah Mostafa <abdallahmostafa6884@gmail.com>
2025-10-07 17:07:29 +0300

    books.js

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,27 +1,3 @@
-// const express = require("express");
-// const Book = require("../models/Book");
-// const auth = require("../middleware/auth");
-
-// const router = express.Router();
-
-// // Add Book
-// router.post("/", auth, async (req, res) => {
-//   const { title, author, year } = req.body;
-//   const book = new Book({ title, author, year });
-//   await book.save();
-//   res.json(book);
-// });
-
-// // Get All Books
-// router.get("/", async (req, res) => {
-//   const books = await Book.find();
-//   res.json(books);
-// });
-
-// module.exports = router;
-
-// ..............................................
-
 const express = require("express");
 const Book = require("../models/Book");
 const auth = require("../middleware/auth");
